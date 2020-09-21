22f237cc596f6f79d230db9a8c80800b52bd76a8
David-Balaian <davidbalayan1@gmail.com>
2020-09-21 14:31:42 +0400

    Updates

---
+++ precache-manifest.aaca871e7eed29a41b987cc85b738bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1230196d74c63a4e31420b511f105998",
+    "url": "/Chess/index.html"
+  },
+  {
+    "revision": "2d30de443ea916ade70e",
+    "url": "/Chess/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "16ea40bc10aeced4ae1c",
+    "url": "/Chess/static/js/2.eeef1994.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Chess/static/js/2.eeef1994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d30de443ea916ade70e",
+    "url": "/Chess/static/js/main.c65ca2a6.chunk.js"
+  },
+  {
+    "revision": "261f9c8d63e3f11f3d55",
+    "url": "/Chess/static/js/runtime-main.c3eb8fa3.js"
+  }
+]);
